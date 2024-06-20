a14905c783728307cd194539eedefb071bf5df67
Franco Gaudiano <53708871+Franco2911@users.noreply.github.com>
2024-06-20 10:12:30 +0200

    Update RouterInterface with phpDoc

--- src/Routing/RouterInterface.php
+++ src/Routing/RouterInterface.php
@@ -5,22 +5,83 @@ namespace AvrestiRouter\Routing;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
+/**
+ * Interface RouterInterface
+ *
+ * Defines the methods that any Router class must implement.
+ */
+
 interface RouterInterface
 {
+    /**
+     * Register a GET route.
+     *
+     * @param string $uri
+     * @param $action
+     * @return Route
+     */
     public function get(string $uri, $action): Route;
 
+    /**
+     * Register a POST route.
+     *
+     * @param string $uri
+     * @param $action
+     * @return Route
+     */
     public function post(string $uri, $action): Route;
 
+    /**
+     * Register a PUT route.
+     *
+     * @param string $uri
+     * @param $action
+     * @return Route
+     */
     public function put(string $uri, $action): Route;
 
+    /**
+     * Register a PATCH route
+     *
+     * @param string $uri
+     * @param $action
+     * @return Route
+     */
     public function patch(string $uri, $action): Route;
 
+    /**
+     * Register a DELETE route
+     *
+     * @param string $uri
+     * @param $action
+     * @return Route
+     */
     public function delete(string $uri, $action): Route;
 
-    public function group(array $attributes, callable $callback);
+    /**
+     * Register a group of routes
+     *
+     * @param array $attributes
+     * @param callable $callback
+     * @return mixed
+     */
+    public function group(array $attributes, callable $callback): mixed;
 
+    /**
+     * Resolves the incoming request to the appropriate route
+     *
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
     public function resolve(ServerRequestInterface $request): ResponseInterface;
 
+    /**
+     * Generates a URL for a named route
+     *
+     * @param string $name
+     * @param array $parameters
+     * @return string
+     */
     public function generateUrl(string $name, array $parameters = []): string;
 
 }
